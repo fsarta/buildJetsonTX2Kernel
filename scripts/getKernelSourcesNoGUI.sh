53c3eb84920ca753d8db8f203b56529ab0333a8b
fsarta <francesco.sarta@gmail.com>
2018-12-21 09:50:01 +0100

    Update getKernelSourcesNoGUI.sh

--- scripts/getKernelSourcesNoGUI.sh
+++ scripts/getKernelSourcesNoGUI.sh
@@ -3,7 +3,7 @@ apt-add-repository universe
 apt-get update
 apt-get install pkg-config -y
 cd /usr/src
-wget https://developer.nvidia.com/embedded/dlc/l4t-sources-31-0-1 -O public_sources.tbz2
+wget https://developer.nvidia.com/embedded/dlc/l4t-jetson-xavier-driver-package-31-1-0 -O public_sources.tbz2
 sudo tar -xvf public_sources.tbz2 public_release/kernel_src.tbz2
 tar -xvf public_release/kernel_src.tbz2
 # Space is tight; get rid of the compressed kernel source
